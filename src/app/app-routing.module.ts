7f8165a66dc71ca4dff7790338a65a05951ba7cd
Martarelli <renanmartarelli@gmail.com>
2022-02-23 14:38:24 -0300

    criado o primeiro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 {
   path: '',
-  component: AppComponent
+  component: HomeComponent
 }
+
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
