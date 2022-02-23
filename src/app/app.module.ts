02628e6e3aa22299f46936c2df5dc5d74d554ad8
Martarelli <renanmartarelli@gmail.com>
2022-02-23 14:53:09 -0300

    ajuste no app modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule, AppRoutingModule, MatToolbarModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
